a5aeb7d1a3ebb52bb7d58a90785aeb01956f5e76
Наседкин Иван <123vanichka321@gmail.com>
2024-08-11 19:46:28 +0300

    some refactoring code

--- ChangeClippingSliderCallback.cpp
+++ ChangeClippingSliderCallback.cpp
@@ -2,14 +2,8 @@
 
 void ChangeClippingSliderCallback::Execute(vtkObject* caller, unsigned long, void*)
 {
-	vtkSP<vtkSliderWidget> sliderWidget = static_cast<vtkSliderWidget*>(caller);
+	vtkSliderWidget *sliderWidget = static_cast<vtkSliderWidget*>(caller);
 	double value = static_cast<vtkSliderRepresentation2D*>(sliderWidget->GetRepresentation())->GetValue();
 	
 	clipPlane->SetOrigin(0, value, 0);
-	
-	for (auto clippingActor : clippingActors)
-	{
-		auto clipper = reinterpret_cast<vtkClipPolyData*>(clippingActor->GetMapper()->GetInput());
-		clipper->SetClipFunction(clipPlane);
-	}
 }
